fafab1c8b130c5f320037ff4a724be084bd432a7
alejandro gallo <alejandroamsg@hotmail.com>
2015-12-18 12:06:32 +0100

    Added option parser for user defined options

---
+++ commands-config.py
@@ -0,0 +1,9 @@
+
+from options import OneLiner
+
+
+OneLiner("let mapleader='-'", "Set mapleader to something else if you like")
+
+OneLiner("inoremap kj <ESC>", "Go to normal mode typing  kj in insert mode")
+
+OneLiner("noremap <leader>ne :NERDTreeToggle<cr>", "Toggle NERDTree file structure easily")
---
+++ options.py
@@ -0,0 +1,62 @@
+import os
+import sys
+
+
+#Change this line if your vim config file is different
+#MYVIMRC=os.path.expanduser("~/.vimrc")
+MYVIMRC="./vimrc"
+
+class OneLiner(object):
+    def __init__(self, option, comment="", auto_run=True):
+        self.auto_run = auto_run
+        self.option = option
+        self.comment = comment
+        if self.auto_run:
+            self.run()
+
+    def getVIMRC(self):
+        """ 
+            Returns file descriptor for the vim configuration file
+        """
+        try:
+            fd = open(MYVIMRC, "a+")
+        except IOError, e: 
+            raise IOError(e)
+            sys.exit(1)
+        else:
+            return fd
+
+
+
+    def checkIfExists(self):
+        vimfd = self.getVIMRC()
+        readline = True
+        while (readline):
+            readline = vimfd.readline()
+            if (self.option in readline) or (readline in self.option):
+                return True
+        return False
+    
+    def write(self):
+        fd = self.getVIMRC()
+        comment = self.comment
+        if comment:
+            if not comment[0]=="\"":
+                comment = "\"" + comment
+        fd.write(comment)
+        fd.write("\n")
+        fd.write(self.option)
+        fd.write("\n")
+
+
+
+    def run(self):
+        if not self.checkIfExists():
+            self.write()
+        else: 
+            print("OneLiner with command \v \t ---> %s \n already exists. \n" %self.option)
+
+        
+
+
+        
