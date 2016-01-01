f0f58526b26ad24d79169a97442a8aefa615561f
alejandro gallo <alejandroamsg@hotmail.com>
2016-01-01 16:25:48 +0100

    Added base class LineCode

--- options.py
+++ options.py
@@ -11,46 +11,66 @@ def vprint(arg1):
     """
     if VERBOSE:
         print(arg1)
-
 #Change this line if your vim config file is different
 MYVIMRC=os.path.expanduser("~/.vimrc")
 
-class OneLiner(object):
-    def __init__(self, option, comment="", auto_run=True):
-        self.auto_run = auto_run
-        self.option = option
-        self.comment = comment
-        if self.auto_run:
-            self.run()
+class CodeLine(object):
 
-    def getVIMRC(self):
+    """This is the main class for lines of code in the vimrc"""
+
+    def __init__(self, line, auto_run):
+        """TODO: to be defined1.
+
+        :auto_run: TODO
+        :line: TODO
+
+        """
+        self._line = line
+        self._auto_run = auto_run
+        if self._auto_run:
+            self.run()
+    def getVIMRC(self, mode="a+"):
         """ 
             Returns file descriptor for the vim configuration file
         """
         try:
-            fd = open(MYVIMRC, "a+")
+            fd = open(MYVIMRC, mode)
         except IOError, e: 
             raise IOError(e)
             sys.exit(1)
         else:
             return fd
-
-
-
     def checkIfExists(self):
-        vimfd = self.getVIMRC()
-        readline = True
-        while (readline):
-            readline = vimfd.readline()
-            if (self.option in readline) :
-                vprint(self.option)
-                vprint(readline)
+        vimfd = self.getVIMRC(mode="r")
+        line = vimfd.readline()
+        while line:
+            line = vimfd.readline()
+            if (self._line in line) :
+                vprint("LINE = "+self._line)
+                vprint("FILE= "+line)
                 vimfd.close()
                 return True
-        vimfd.close()
         return False
-    
     def write(self):
+        fd = self.getVIMRC()
+        fd.write(self._line)
+        fd.write("\n")
+        fd.close()
+    def run(self):
+        if not self.checkIfExists():
+            self.write()
+        else: 
+            print("Line with command \v \t ---> %s \n already exists. \n" %self._line)
+        
+class OneLiner(CodeLine):
+    def __init__(self, option, comment="", auto_run=True):
+        self.auto_run = auto_run
+        self.option = option
+        self.comment = comment
+        # We call the initialization of OneLiner here because of auto_run
+        super(OneLiner, self).__init__(option, auto_run)
+    def write(self):
+        # Override super write method
         fd = self.getVIMRC()
         comment = self.comment
         if comment:
@@ -61,14 +81,6 @@ class OneLiner(object):
         fd.write(self.option)
         fd.write("\n")
         fd.close()
-
-
-
-    def run(self):
-        if not self.checkIfExists():
-            self.write()
-        else: 
-            print("OneLiner with command \v \t ---> %s \n already exists. \n" %self.option)
 
 class ManyOneLiners(object):
 
@@ -108,3 +120,5 @@ class ManyOneLiners(object):
             self.write()
         else: 
             print(" ManyOneLiners with command \v \t ---> %s \n already exists. \n" %self.options)
+
+
