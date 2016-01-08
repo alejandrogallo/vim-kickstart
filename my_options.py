cb902d8c8419abb385d17042af718c831d8df59a
alejandro gallo <alejandroamsg@hotmail.com>
2016-01-08 17:04:42 +0100

    Buffer tab corrected

--- my_options.py
+++ my_options.py
@@ -22,13 +22,14 @@ OneLiner("nnoremap <leader>s :w<cr>", "Save current buffer")
 OneLiner("nnoremap <leader>q :q<cr>", "Close vim without saving")
 OneLiner("nnoremap <leader>f /", "/ is way too far away")
 OneLiner("set incsearch", "Highlight search as you type the searching word")
-# OneLiner(":nmap \t :set expandtab tabstop=4 shiftwidth=4 softtabstop=4<CR>", "Set tab to 4")
-# OneLiner(":nmap \T :set expandtab tabstop=8 shiftwidth=8 softtabstop=4<CR>", "Set tab to 4")
-# OneLiner(":nmap \M :set noexpandtab tabstop=8 softtabstop=4 shiftwidth=4<CR>", "Set tab to 4")
-# OneLiner(":nmap \m :set expandtab tabstop=2 shiftwidth=2 softtabstop=2<CR>", "Set tab to 4")
 OneLiner("set wildmenu", "provide a graphical menu of all the matches you can cycle through in the command line")
+
+####################
+#  BUFFER CONTROL  #
+####################
+
 OneLiner("noremap <Tab> :bn<CR>","Change to next buffer") 
-OneLiner("noremap <Tab-S> :bp<CR>","Change to previous buffer") 
+OneLiner("noremap <S-Tab> :bp<CR>","Change to previous buffer") 
 
 
 ######################
@@ -52,3 +53,17 @@ ManyOneLiners([
 'let g:UltiSnipsEditSplit="vertical"'], "ULTISNIPS CONFIGURATION")
 
 
+################
+#  TABs stuff  #
+################
+
+OneLiner("set tabstop=2", "Set tabstop to tell vim how many columns a tab counts for")
+OneLiner("set expandtab", "When expandtab is set, hitting Tab in insert mode will produce the appropriate number of spaces.")
+OneLiner("set shiftwidth=2", "Set shiftwidth to control how many columns text is indented with the reindent operations (<< and >>)")
+OneLiner("set softtabstop=2", "Set softtabstop to control how many columns vim uses when you hit Tab in insert mode")
+
+# COMMON CONFIGURATIONS
+OneLiner(r":nmap \t :set expandtab tabstop=4 shiftwidth=4 softtabstop=4<CR>", "Set tab to 4")
+OneLiner(":nmap \T :set expandtab tabstop=8 shiftwidth=8 softtabstop=4<CR>", "Set tab to 8 with insertmode tab to 4")
+OneLiner(":nmap \M :set noexpandtab tabstop=8 softtabstop=4 shiftwidth=4<CR>", "Some other common configuration")
+OneLiner(":nmap \m :set expandtab tabstop=2 shiftwidth=2 softtabstop=2<CR>", "Set tab to 2")
