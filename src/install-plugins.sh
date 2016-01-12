650a31a650e318f083b1852aaa13d17201f2bc03
Alejandro Gallo <gallo@pccq05.fkf.mpg.de>
2016-01-12 17:20:33 +0100

    Updated install-pluding, tidied up code

--- src/install-plugins.sh
+++ src/install-plugins.sh
@@ -14,13 +14,13 @@ VIM_INSTALL_FOLDER="$VIM_FOLDER/bundle"
 PLUGIN_INSTALLED=""
 if [ "$(uname)" == "Darwin"  ] 
 then
-	__MAC__="TRUE"
-	echo "We are on a MAC, yeah"
-	echo "We use brew for some stuff.. Maybe you should uninstall MacPorts and Fink if they are on the system."
-	echo "Checking for HomeBrew.."
-	check_brew
+  __MAC__="TRUE"
+  echo "We are on a MAC, yeah"
+  echo "We use brew for some stuff.. Maybe you should uninstall MacPorts and Fink if they are on the system."
+  echo "Checking for HomeBrew.."
+  check_brew
 else
-	__LINUX__="TRUE"
+  __LINUX__="TRUE"
 fi
 
 
@@ -30,56 +30,63 @@ fi
 
 
 begin_install() {
-	installing $plug_name
-	if test -d $VIM_INSTALL_FOLDER/$folder_name; then 
-		already_installed $plug_name
-		PLUGIN_INSTALLED="True"
-	else
-		PLUGIN_INSTALLED=""
-	fi
+  installing $plug_name
+  if test -d $VIM_INSTALL_FOLDER/$folder_name; then 
+    already_installed $plug_name
+    PLUGIN_INSTALLED="True"
+  else
+    PLUGIN_INSTALLED=""
+  fi
 }
+
 check_brew(){
-	if [ $(which brew)  ]
-	then 
-		echo "Brew detected"
-	else 
-		echo "Brew not detected, install it, go to http://brew.sh"
-		#ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-		exit 1
-	fi
+  if [ $(which brew)  ]
+  then 
+    echo "Brew detected"
+  else 
+    echo "Brew not detected, install it, go to http://brew.sh"
+    #ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    exit 1
+  fi
 }
+
 installing(){
-	echo -e "\n\n====> INSTALLING $1 <===="
+  echo -e "\n\n====> INSTALLING $1 <===="
 }
+
 already_installed(){
-	echo -e "\t---> Package $1 already installed \n\t\t(erase the files in $VIM_INSTALL_FOLDER/ to trigger the installation)"
+  echo -e "\t---> Package $1 already installed \n\t\t(erase the files in $VIM_INSTALL_FOLDER/ to trigger the installation)"
 }
+
 add_plugin(){
-	if test -n "$1"; then 
-		link=$1
-	else
-		link=$plug_name
-	fi
-	echo "Adding plugin $link to .vimrc"
-	cp .vimrc .vimrc.tmp
-	cat .vimrc | perl -pe "s/(.*vundle#begin.*)/\1\nPlugin \'$link\'/g" > .vimrc.tmp
-	#cat .vimrc.tmp
-	mv .vimrc.tmp .vimrc
+  if test -n "$1"; then 
+    link=$1
+  else
+    link=$plug_name
+  fi
+  echo "Adding plugin $link to .vimrc"
+  cp .vimrc .vimrc.tmp
+  cat .vimrc | perl -pe "s/(.*vundle#begin.*)/\1\nPlugin \'$link\'/g" > .vimrc.tmp
+  #cat .vimrc.tmp
+  mv .vimrc.tmp .vimrc
 
 }
 
 add_config() {
-	text=$@
-	echo $@ >> $VIMRC	
+  text=$@
+  echo $@ >> $VIMRC	
 }
 
 install_with_vundle(){
-	echo "Calling Vundle installation routine"
-	vim +PluginInstall +qall 
+  echo "Calling Vundle installation routine"
+  vim +PluginInstall +qall 
 }
 
 
-VUNDLE_URL="https://github.com/VundleVim/Vundle.vim"
+###########################################
+#  CONTROL OF VIM FOLDERS AND CONF FILES  #
+###########################################
+
 ORIGIN=$PWD
 echo "CD to $HOME"
 cd $HOME
@@ -87,17 +94,17 @@ cd $HOME
 # CONTROL OF DIRECTORIES 
 if test -d $VIM_FOLDER
 then 
-	echo "Folder .vim detected..."
-	if test -d $VIM_INSTALL_FOLDER
-	then
-		echo "Folder $VIM_INSTALL_FOLDER detected..."
-	else
-		echo "Folder $VIM_INSTALL_FOLDER not detected... Creating it"
-		mkdir $VIM_INSTALL_FOLDER
-	fi
+  echo "Folder .vim detected..."
+  if test -d $VIM_INSTALL_FOLDER
+  then
+    echo "Folder $VIM_INSTALL_FOLDER detected..."
+  else
+    echo "Folder $VIM_INSTALL_FOLDER not detected... Creating it"
+    mkdir $VIM_INSTALL_FOLDER
+  fi
 else
-	echo "Creating .vim folder..."
-	mkdir .vim
+  echo "Creating .vim folder..."
+  mkdir .vim
 fi
 
 
@@ -109,22 +116,22 @@ VUNDLE_URL="https://github.com/VundleVim/Vundle.vim"
 installing Vundle.vim
 if test -e $VIM_INSTALL_FOLDER/Vundle.vim
 then
-    already_installed Vundle.vim
+  already_installed Vundle.vim
 else	
-	echo "Vundle.vim not detected"
-	echo "Getting Vundle from $VUNDLE_URL"
-	git clone $VUNDLE_URL $VIM_INSTALL_FOLDER/Vundle.vim
-	add_config "\" VUNDLE.VIM CONFIGURATION BEGIN"
-	add_config "set nocompatible"
-	add_config "filetype off"
-	add_config "set rtp+=$VIM_INSTALL_FOLDER/Vundle.vim"
-	add_config "call vundle#begin()"
-	add_config "Plugin 'VundleVim/Vundle.vim'"
-	add_config "\" All of your Plugins must be added before the following line"
-	add_config "call vundle#end()"
-	add_config "\" VUNDLE.VIM CONFIGURATION END"
-	add_config ""
-	install_with_vundle
+  echo "Vundle.vim not detected"
+  echo "Getting Vundle from $VUNDLE_URL"
+  git clone $VUNDLE_URL $VIM_INSTALL_FOLDER/Vundle.vim
+  add_config "\" VUNDLE.VIM CONFIGURATION BEGIN"
+  add_config "set nocompatible"
+  add_config "filetype off"
+  add_config "set rtp+=$VIM_INSTALL_FOLDER/Vundle.vim"
+  add_config "call vundle#begin()"
+  add_config "Plugin 'VundleVim/Vundle.vim'"
+  add_config "\" All of your Plugins must be added before the following line"
+  add_config "call vundle#end()"
+  add_config "\" VUNDLE.VIM CONFIGURATION END"
+  add_config ""
+  install_with_vundle
 fi
 
 
@@ -136,29 +143,28 @@ fi
 #installing YouCompleteMe
 #if [ $__MAC__ ]
 #then 
-	#echo "We need MacVim"
-	#if [ $(which mvim) ]
-	#then 
-		#echo "MacVim (mvim) detected..."
-	#else 
-		#echo "MacVim (mvim) not detected... Installing it"
-		#brew install MacVim
-	#fi
+#echo "We need MacVim"
+#if [ $(which mvim) ]
+#then 
+#echo "MacVim (mvim) detected..."
+#else 
+#echo "MacVim (mvim) not detected... Installing it"
+#brew install MacVim
 #fi
-
+#fi
 
 #if test -e $VIM_INSTALL_FOLDER/YouCompleteMe
 #then
-   #already_installed YouCompleteMe
+#already_installed YouCompleteMe
 #else
-   #echo "YouCompleteMe not detected, getting it from $YCM_URL"	
-	#git clone $YCM_URL $VIM_INSTALL_FOLDER/YouCompleteMe
-	#cd $VIM_INSTALL_FOLDER/YouCompleteMe
-	#git submodule update --init --recursive
-	#python install.py --clang-completer
-	#cd ~ 
-	#add_plugin "Valloric\/YouCompleteMe"
-	#install_with_vundle
+#echo "YouCompleteMe not detected, getting it from $YCM_URL"	
+#git clone $YCM_URL $VIM_INSTALL_FOLDER/YouCompleteMe
+#cd $VIM_INSTALL_FOLDER/YouCompleteMe
+#git submodule update --init --recursive
+#python install.py --clang-completer
+#cd ~ 
+#add_plugin "Valloric\/YouCompleteMe"
+#install_with_vundle
 
 #fi
 
@@ -168,31 +174,24 @@ fi
 #  NerdTree  #
 ##############
 
-NERDTREE_URL="https://github.com/scrooloose/nerdtree.git"
-installing NERDTree
-if test -d $VIM_INSTALL_FOLDER/nerdtree
-then
-	already_installed nerdtree
-else	
-	git clone $NERDTREE_URL $VIM_INSTALL_FOLDER/nerdtree
-	add_plugin "scrooloose\/nerdtree"
-	install_with_vundle
+folder_name="nerdtree"
+plug_name="scrooloose\/nerdtree"
+begin_install
+if test -z "$PLUGIN_INSTALLED"; then
+  add_plugin 
+  install_with_vundle
 fi
 
 ##############
 #  AIRLINE   #
 ##############
 
-AIRLINE_URL="https://github.com/bling/vim-airline.git"
 folder_name="vim-airline"
 plug_name="bling\/vim-airline"
 begin_install
 if test -z "$PLUGIN_INSTALLED"; then
-	git clone $AIRLINE_URL $VIM_INSTALL_FOLDER/$folder_name
-	add_plugin $plug_name
-	# for color in the tabline
-	add_config "set term=xterm-256color"
-	install_with_vundle
+  add_plugin 
+  install_with_vundle
 fi
 
 
@@ -204,11 +203,9 @@ folder_name="emmet-vim"
 plug_name="mattn\/emmet-vim"
 begin_install
 if test -z "$PLUGIN_INSTALLED"; then
-	add_plugin $plug_name
-	install_with_vundle
+  add_plugin $plug_name
+  install_with_vundle
 fi
-
-
 
 
 
@@ -220,12 +217,22 @@ folder_name="ultisnips"
 plug_name="SirVer\/ultisnips"
 begin_install
 if test -z "$PLUGIN_INSTALLED"; then
-	add_plugin 
-	add_plugin 'honza\/vim-snippets'
-	install_with_vundle
+  add_plugin
+  install_with_vundle
 fi
 
 
+##################
+#  VIM-SNIPPETS  #
+##################
+
+folder_name="vim-snippets"
+plug_name='honza\/vim-snippets'
+begin_install
+if test -z "$PLUGIN_INSTALLED"; then
+  add_plugin
+  install_with_vundle
+fi
 
 
 ################################
@@ -236,8 +243,8 @@ folder_name="nerdcommenter"
 plug_name="scrooloose\/nerdcommenter"
 begin_install
 if test -z "$PLUGIN_INSTALLED"; then
-	add_plugin 
-	install_with_vundle 
+  add_plugin 
+  install_with_vundle 
 fi
 
 
